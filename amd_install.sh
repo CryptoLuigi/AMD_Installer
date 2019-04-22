9480e8947de8a0aaa8a3e1f1db5d22321b93ac1a
Michael R <48301357+CryptoLuigi@users.noreply.github.com>
2019-04-22 11:48:42 -0400

    Create amd_install.sh

---
+++ amd_install.sh
@@ -0,0 +1,29 @@
+# Automatic installation of Autoswitch on HiveOS
+---
+## By: CryptoLuigi
+
+### This script is designed to automatically install the autoswitching program for nicehash.
+#### Click link below to learn more about HiveOS
+
+https://hiveos.farm?ref=84706
+
+Promocode for $10: CRYPTOMIKE
+
+When running this script, the script will ask for your HiveOS API keys.
+Then the program will ask for the hashrate benchmarks for each support algorithm
+The data will be entered into autoswitch conf file
+Once autoswitch is configured the script will ask the user if they want to start the program.
+
+```
+Further documentation can be found on the HiveOS forum
+ ```
+ 
+ 
+For a guided installation enter the following 3 commands
+```
+wget https://raw.githubusercontent.com/CryptoLuigi/autoswitch_autosetup/master/autoswitch_autosetup.sh
+
+chmod +x ./autoswitch_autosetup.sh
+
+./autoswitch_autosetup.sh
+ ```
